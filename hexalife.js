55a463d9540e44a12a0c83b0e63b1d4535409b37
Cedrick <ced.lemay@hotmail.com>
2025-07-20 23:34:28 -0400

    Loops and fills

--- hexalife.js
+++ hexalife.js
@@ -3,23 +3,51 @@ const ctx = canvas.getContext('2d');
 
 const _sides = 6;
 const _angle = 2 * Math.PI / _sides;
-const _radius = 50;
+const _radius = 20;
+
+const _lines = 6;
+const _columns = 8;
+
+/**
+ * 
+ *  X dist between  = r*sqrt(3)
+ * 
+ *  Y dist between  = 3/2*r
+ * 
+ *  x offset by r*sqrt(3)/2 each 2 line
+ * 
+ */
 
 function init() {
-    drawShape(_radius,_radius);
-    drawShape(_radius + _radius*Math.sqrt(3),_radius);
-    drawShape(_radius + 2*_radius*Math.sqrt(3),_radius);
 
-    drawShape(_radius + _radius*Math.sqrt(3)/2,2.5*_radius);
-    drawShape(_radius + 1.5*_radius*Math.sqrt(3),2.5*_radius);
-    drawShape(_radius + 2.5*_radius*Math.sqrt(3),2.5*_radius);
+
+    let xOffset = 0;
+    let yOffset = 0;
+
+    for(let i = 0; i < _lines; i++)
+    {
+        yOffset = i * _radius * 1.5;
+        for(let j = 0; j < _columns; j++)
+        {
+            xOffset = _radius * Math.sqrt(3) * (j + (i%2)/2);            
+            drawShape(_radius + xOffset,_radius + yOffset,(i+j)%2);
+        }   
+    }
+
+    // drawShape(_radius,_radius);
+    // drawShape(_radius + _radius*Math.sqrt(3),_radius);
+    // drawShape(_radius + 2*_radius*Math.sqrt(3),_radius);
+
+    // drawShape(_radius + _radius*Math.sqrt(3)/2,2.5*_radius);
+    // drawShape(_radius + 1.5*_radius*Math.sqrt(3),2.5*_radius);
+    // drawShape(_radius + 2.5*_radius*Math.sqrt(3),2.5*_radius);
 
 
     
 }
 init();
 
-function drawShape(x,y)
+function drawShape(x,y,f)
 {
     ctx.beginPath();
     for (var i = 0; i < _sides; i++) {
@@ -28,5 +56,8 @@ function drawShape(x,y)
 
       ctx.closePath();
     
-    ctx.stroke();
+    if(f == 0)
+        ctx.stroke();
+    else
+        ctx.fill()
 }
