3ac6b1d4861794f3be49b8f88e45d41b69e92a72
Cedrick <ced.lemay@hotmail.com>
2025-07-23 21:19:05 -0400

    Pre change state

--- hexalife.js
+++ hexalife.js
@@ -3,10 +3,12 @@ const ctx = canvas.getContext('2d');
 
 const _sides = 6;
 const _angle = 2 * Math.PI / _sides;
-const _radius = 20;
+const _radius = 50;
 
 const _lines = 6;
 const _columns = 8;
+
+const arr = new Array(_lines*_columns).fill(0);
 
 /**
  * 
@@ -20,7 +22,6 @@ const _columns = 8;
 
 function init() {
 
-
     let xOffset = 0;
     let yOffset = 0;
 
@@ -31,33 +32,51 @@ function init() {
         {
             xOffset = _radius * Math.sqrt(3) * (j + (i%2)/2);            
             drawShape(_radius + xOffset,_radius + yOffset,(i+j)%2);
+
+            //TEMP            
+            addTextInShape(i,j,_radius + xOffset,_radius + yOffset);
+            //TEMP            
+
         }   
     }
 
-    // drawShape(_radius,_radius);
-    // drawShape(_radius + _radius*Math.sqrt(3),_radius);
-    // drawShape(_radius + 2*_radius*Math.sqrt(3),_radius);
+    canvas.addEventListener('click', setState);
 
-    // drawShape(_radius + _radius*Math.sqrt(3)/2,2.5*_radius);
-    // drawShape(_radius + 1.5*_radius*Math.sqrt(3),2.5*_radius);
-    // drawShape(_radius + 2.5*_radius*Math.sqrt(3),2.5*_radius);
-
-
-    
 }
+
 init();
+
+
 
 function drawShape(x,y,f)
 {
     ctx.beginPath();
     for (var i = 0; i < _sides; i++) {
-        ctx.lineTo(x + _radius * Math.cos(_angle * (i+0.5)), y + _radius * Math.sin(_angle * (i+0.5)));
-      }
+        ctx.lineTo(x + _radius * Math.cos(_angle * (i + 0.5)), y + _radius * Math.sin(_angle * (i + 0.5)));
+    }
 
-      ctx.closePath();
+    ctx.closePath();
     
-    if(f == 0)
+    //if(f == 0)
         ctx.stroke();
-    else
-        ctx.fill()
+    //else
+    //    ctx.fill()
+    
 }
+
+function setState(e)
+{
+   
+
+}
+
+//TEMP
+//TEMP
+//TEMP
+function addTextInShape(i,j,x,y)
+{
+    ctx.fillText(i+"-"+j,x-7,y+3);
+
+}
+
+//TEMP
