9ce3feb893102f19fabdbe1314fe8aa1589bb6d5
Cedrick <ced.lemay@hotmail.com>
2025-07-25 13:11:25 -0400

    Approximate X

--- hexalife.js
+++ hexalife.js
@@ -8,7 +8,25 @@ const _radius = 50;
 const _lines = 6;
 const _columns = 8;
 
-const arr = new Array(_lines*_columns).fill(0);
+
+
+const sqrt3 = Math.sqrt(3);
+
+const _state = new Array(_lines*_columns).fill(0);
+
+const xyMargin = 5;
+const x0 = (_radius * sqrt3 / 2) + xyMargin;
+const y0 = _radius + xyMargin;
+
+
+function init() {
+
+    drawGrid();
+    canvas.addEventListener('click', setState);
+
+}
+
+init();
 
 /**
  * 
@@ -19,19 +37,22 @@ const arr = new Array(_lines*_columns).fill(0);
  *  x offset by r*sqrt(3)/2 each 2 line
  * 
  */
-
-function init() {
+function drawGrid()
+{    
+    clearGrid();
 
     let xOffset = 0;
     let yOffset = 0;
+    
+
 
     for(let i = 0; i < _lines; i++)
     {
         yOffset = i * _radius * 1.5;
         for(let j = 0; j < _columns; j++)
         {
-            xOffset = _radius * Math.sqrt(3) * (j + (i%2)/2);            
-            drawShape(_radius + xOffset,_radius + yOffset,(i+j)%2);
+            xOffset = _radius * sqrt3 * (j + (i%2)/2);            
+            drawShape(x0 + xOffset, y0 + yOffset, _state[i*_columns + j]);
 
             //TEMP            
             addTextInShape(i,j,_radius + xOffset,_radius + yOffset);
@@ -39,35 +60,53 @@ function init() {
 
         }   
     }
-
-    canvas.addEventListener('click', setState);
-
 }
 
-init();
+function clearGrid()
+{
+    ctx.clearRect(0,0,canvas.width,canvas.height);
+}
 
-
-
-function drawShape(x,y,f)
+function drawShape(x,y,s)
 {
     ctx.beginPath();
-    for (var i = 0; i < _sides; i++) {
+    for (var i = 0; i < 6; i++) {
+
+        let _t = x + _radius * Math.cos(_angle * (i + 0.5));
+
         ctx.lineTo(x + _radius * Math.cos(_angle * (i + 0.5)), y + _radius * Math.sin(_angle * (i + 0.5)));
     }
 
     ctx.closePath();
     
-    //if(f == 0)
+    if(s == 0)
         ctx.stroke();
-    //else
-    //    ctx.fill()
+    else
+       ctx.fill()
     
 }
 
 function setState(e)
 {
-   
+    
 
+    let posX = e.clientX;
+    let posY = e.clientY;
+
+
+    //approximate X
+
+    let evenXIndex = (posX-xyMargin) / (sqrt3 * _radius);
+    let oddXIndex = (posX - (_radius * sqrt3 / 2) - xyMargin) / (sqrt3 * _radius);
+
+    evenXIndex = Math.floor(evenXIndex);
+    oddXIndex = Math.floor(oddXIndex );
+
+    alert("even X : " + evenXIndex + "   odd X : " + oddXIndex);
+
+    return;
+
+    drawGrid()
 }
 
 //TEMP
